5aea3c2becc56ed421b1366063bf833e1675ae20
mojahid-belaman <mojahidbelaman@gmail.com>
2022-02-28 16:08:06 +0100

    add class list random in DIV

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
-let myPara = document.getElementById('para');
-let data = myPara.getAttribute('data-para');
+let myPara = document.getElementById('para'),
+    data = myPara.getAttribute('data-para'),
+    myDiv = document.querySelector('.container'),
+    myArr = ["class-one", "class-two", "class-three", "class-four"],
+    randomKey = Math.floor(Math.random() * myArr.length);
 
-(function Effect_Text() {
-    let i = 0;
-    window.onload = function () {
-        var myVar = setInterval(function () {
-            myPara.textContent += data[i];
-            i++;
-            if (i > data.length - 1)
-                clearInterval(myVar);
+    
+    (function Effect_Text() {
+        let i = 0;
+        window.onload = function () {
+            
+            myDiv.classList.toggle(myArr[randomKey]);
+            var myVar = setInterval(function () {
+                myPara.textContent += data[i];
+                i++;
+                if (i > data.length - 1)
+                    clearInterval(myVar);
         }, 50)
     }
 
